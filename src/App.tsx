c8eb89a3930e0ffc2604602ba104b1c835981442
Anderson Menezes <anderson.stifip@gmail.com>
2024-04-25 05:22:54 -0300

    feat: instalação e configuração do moment para manipular datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import moment from "moment";
+import "moment/dist/locale/pt-br";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 import { Toaster } from "sonner";
@@ -8,6 +10,8 @@ import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
+  moment.locale("pt-br");
+
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
